a3200a2e8cda56de75be4643b61eff0b4b50af8b
Roy Cornelissen <roy_cornelissen@hotmail.com>
2014-11-14 07:25:38 +0100

    Todo app with SQLite database
    Based on Todo Xamarin.Forms sample

---
+++ Todo/PCL/Todo/Views/TodoListPage.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Todo
+{
+	public class TodoListPage : ContentPage
+	{
+		ListView listView;
+		public TodoListPage ()
+		{
+			Title = "Todo";
+
+			listView = new ListView ();
+			listView.ItemTemplate = new DataTemplate 
+					(typeof (TodoItemCell));
+			listView.ItemSelected += (sender, e) => {
+				var todoItem = (TodoItem)e.SelectedItem;
+				var todoPage = new TodoItemPage();
+				todoPage.BindingContext = todoItem;
+				Navigation.PushAsync(todoPage);
+			};
+
+			var layout = new StackLayout();
+			if (Device.OS == TargetPlatform.WinPhone) { // WinPhone doesn't have the title showing
+				layout.Children.Add(new Label{
+					Text="Todo", 
+					Font=Font.SystemFontOfSize (NamedSize.Large)});
+			}
+			layout.Children.Add(listView);
+			layout.VerticalOptions = LayoutOptions.FillAndExpand;
+			Content = layout;
+
+			#region toolbar
+			ToolbarItem tbi = null;
+			if (Device.OS == TargetPlatform.iOS)
+			{
+				tbi = new ToolbarItem("+", null, () =>
+					{
+						var todoItem = new TodoItem();
+						var todoPage = new TodoItemPage();
+						todoPage.BindingContext = todoItem;
+						Navigation.PushAsync(todoPage);
+					}, 0, 0);
+			}
+			if (Device.OS == TargetPlatform.Android) { // BUG: Android doesn't support the icon being null
+				tbi = new ToolbarItem ("+", "plus", () => {
+					var todoItem = new TodoItem();
+					var todoPage = new TodoItemPage();
+					todoPage.BindingContext = todoItem;
+					Navigation.PushAsync(todoPage);
+				}, 0, 0);
+			}
+			if (Device.OS == TargetPlatform.WinPhone)
+			{
+				tbi = new ToolbarItem("Add", "add.png", () =>
+					{
+						var todoItem = new TodoItem();
+						var todoPage = new TodoItemPage();
+						todoPage.BindingContext = todoItem;
+						Navigation.PushAsync(todoPage);
+					}, 0, 0);
+			}
+
+			ToolbarItems.Add (tbi);
+
+			#endregion
+		}
+
+		protected override void OnAppearing ()
+		{
+			base.OnAppearing ();
+			listView.ItemsSource = App.Database.GetItems ();
+		}
+	}
+}
+
